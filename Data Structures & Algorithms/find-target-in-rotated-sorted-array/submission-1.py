33519c0a2d761c3786cc8745fc50fa05ce9d346e
gj5wang <gj5wang@uwaterloo.ca>
2026-06-18 10:44:42 -0300

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        #logic is that u try to find which group the target belongs to
+        #on the left side or the right side
+
+        while(left <= right):
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            #left half is sorted
+            if nums[left] <= nums[mid]: #left half is sorted
+                #target lies in left half
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            
+            #right half is sorted if left half is not sorted
+            else:
+                #target lies in right half
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return -1
+
+
+
+        
+
+
+
+
+        
